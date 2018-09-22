fa382cd955f69414345bbd84262fbbec1eaa73d2
Nara Mothukuri <nmothukuri@gmail.com>
2018-09-22 17:33:55 -0400

    initial commit

---
+++ servicebroker-mongo/src/main/java/com/aacloud/servicebrokermongo/ServicebrokerMongoApplication.java
@@ -0,0 +1,12 @@
+package com.aacloud.servicebrokermongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicebrokerMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicebrokerMongoApplication.class, args);
+	}
+}
